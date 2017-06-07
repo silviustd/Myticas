d78b1c909168f13ac253b05bd56e2aefc83f8560
silviustd <silviu.stochita@gmail.com>
2017-06-06 21:17:31 -0400

    Added MergeSortAlghorimt

--- Myticas/MainWindow.xaml.cs
+++ Myticas/MainWindow.xaml.cs
@@ -28,11 +28,9 @@ namespace Myticas
         private void btnSort_Click(object sender, RoutedEventArgs e)
         {
             int[] numbers = new int[1];
-            //mergesort(numbers, new int[numbers.Length], 0, numbers.Length - 1);
             String numberse = textBoxU.Text;
-            //string[] strings = numberse.Split(',',StringSplitOptions.RemoveEmptyEntries);
-            string[] strings = numberse.Split(new String[] {","}, StringSplitOptions.RemoveEmptyEntries);
             
+            string[] strings = numberse.Split(new String[] {","}, StringSplitOptions.RemoveEmptyEntries);
             int i = 0;
             foreach (String item in strings)
             {
@@ -42,69 +40,13 @@ namespace Myticas
                 i++;
                 Array.Resize(ref numbers, i+1);
             }
-            MergeSort(numbers, 0, numbers.Length - 1);
+            MergeSortAlghoritm.MergeSort(numbers, 0, numbers.Length - 1);
             var resultSort = string.Join(",", numbers);
             textBoxS.Text = resultSort;
             
         }
 
-        static public void MergeSort(int[] numbers, int leftStart, int rightEnd)
-        {
-            int mid;
 
-            if (rightEnd > leftStart)
-            {
-                mid = (rightEnd + leftStart) / 2;
-
-                Task[] tasks = new Task[2];
-
-                tasks[0] = Task.Factory.StartNew(() => MergeSort(numbers, leftStart, mid));
-                tasks[1] = Task.Factory.StartNew(() => MergeSort(numbers, (mid + 1), rightEnd));
-
-                tasks[0].Wait();
-                tasks[1].Wait();
-
-                MergeHalves(numbers, leftStart, (mid + 1), rightEnd);
-            }
-        }
-
-        static public void MergeHalves(int[] numbers, int left, int mid, int right)
-        {
-
-            int[] temp = new int[25];
-            int i, leftEnd, rightStart, index;
-
-            leftEnd = (mid - 1);
-            index = left;
-            rightStart = (right - left + 1);
-
-            while ((left <= leftEnd) && (mid <= right))
-            {
-
-                if (numbers[left] <= numbers[mid])
-                    temp[index++] = numbers[left++];
-                else
-                    temp[index++] = numbers[mid++];
-            }
-
-
-
-            while (left <= leftEnd)
-                temp[index++] = numbers[left++];
-
-
-            while (mid <= right)
-                temp[index++] = numbers[mid++];
-
-
-
-            for (i = 0; i < rightStart; i++)
-            {
-                numbers[right] = temp[right];
-                right--;
-            }
-
-        }
 
     }
 }
---
+++ Myticas/MergeSort.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myticas
+{
+    class MergeSortAlghoritm
+    {
+        public static void MergeSort(int[] numbers, int leftStart, int rightEnd)
+        {
+            int mid;
+
+            if (rightEnd > leftStart)
+            {
+                mid = (rightEnd + leftStart) / 2;
+
+                Task[] tasks = new Task[2];
+
+                tasks[0] = Task.Factory.StartNew(() => MergeSort(numbers, leftStart, mid));
+                tasks[1] = Task.Factory.StartNew(() => MergeSort(numbers, (mid + 1), rightEnd));
+
+                tasks[0].Wait();
+                tasks[1].Wait();
+
+                MergeHalves(numbers, leftStart, (mid + 1), rightEnd);
+            }
+        }
+
+        public static void MergeHalves(int[] numbers, int left, int mid, int right)
+        {
+
+            int[] temp = new int[25];
+            int i, leftEnd, rightStart, index;
+
+            leftEnd = (mid - 1);
+            index = left;
+            rightStart = (right - left + 1);
+
+            while ((left <= leftEnd) && (mid <= right))
+            {
+
+                if (numbers[left] <= numbers[mid])
+                    temp[index++] = numbers[left++];
+                else
+                    temp[index++] = numbers[mid++];
+            }
+
+
+
+            while (left <= leftEnd)
+                temp[index++] = numbers[left++];
+
+
+            while (mid <= right)
+                temp[index++] = numbers[mid++];
+
+
+
+            for (i = 0; i < rightStart; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+
+        }
+    }
+}
